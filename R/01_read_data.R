efa7c5a3057ef6419efd0bccfb57af404314ad6f
mgenu <mathieu.genu@univ-lr.fr>
2019-12-05 17:57:58 +0100

    get good pratique file

--- R/01_read_data.R
+++ R/01_read_data.R
@@ -9,10 +9,8 @@ parcelle_raw <- read.csv(file.path(path_file,parcelle_file), h=T, row.names = 1)
 parcelle_raw <- as_tibble(parcelle_raw)
 flore_raw <- read.csv(file.path(path_file,flore_file), h=T, sep = ";")
 flore_raw <- as_tibble(flore_raw)
-pratique_raw <- read.csv(file.path(path_file,flore_file), h=T, sep = ";")
+pratique_raw <- read.csv(file.path(path_file,pratique_file), h=T, sep = ",")
 pratique_raw <- as_tibble(pratique_raw)
 rm(flore_file, parcelle_file, path_file, pratique_file)
-
-
 
                          
