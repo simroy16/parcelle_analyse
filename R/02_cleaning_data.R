db00bb5745f19137be6791dc6b613291927e7850
Simon ROY <roysimon@supagro.inra.lan>
2019-12-05 17:46:51 +0100

    split first column of flore into 2 columns annee et parcelle_id

--- R/02_cleaning_data.R
+++ R/02_cleaning_data.R
@@ -1,12 +1,8 @@
 
 source("R/01_read_data.R")
 
-# delete first column
-parcelle_raw <- parcelle_raw[,-1]
-
 
 # cleaning parcelle data
-
 parcelle <- parcelle_raw[,c("ID_Parcelle", "region", "code_parcelle.x", "plante_reference", "coord_x", "coord_y", 
                             "climat", "typo_pays_principale",
                             "topographie", "altitude", "pourcent_pente", "surface", 
@@ -14,5 +10,14 @@ parcelle <- parcelle_raw[,c("ID_Parcelle", "region", "code_parcelle.x", "plante_
                             "pct_limon", "pct_caillou", "pct_calcaire", "type_bordure", "annee_releve", 
                             "largeur_bordure")]
 
-# split first column of flore_raw
 
+# split first column of flore_raw
+annee <- vector()
+parcelle_id <- vector()
+
+for(l in 1: length(flore_raw$IDy)) {
+  annee <- c(annee, substring(flore_raw$IDy[l], first = nchar(as.character(flore_raw$IDy[l]))-3))
+  parcelle_id <- c(parcelle_id, substr(flore_raw$IDy[l], stop = nchar(as.character(flore_raw$IDy[l]))-5, start = 0))
+}
+flore_raw$annee <- annee
+flore_raw$parcelle_id <- parcelle_id
