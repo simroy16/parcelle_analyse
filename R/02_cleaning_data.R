940f6082d542b19d722d15976d0e61a36063b0cf
Simon ROY <roysimon@supagro.inra.lan>
2019-12-06 09:56:20 +0100

    Removing some columns in 'parcelle' and some rows - Cleaning data

--- R/02_cleaning_data.R
+++ R/02_cleaning_data.R
@@ -5,10 +5,10 @@ source("R/01_read_data.R")
 # cleaning parcelle data
 parcelle <- parcelle_raw[,c("ID_Parcelle", "region", "code_parcelle.x", "plante_reference", "coord_x", "coord_y", 
                             "climat", "typo_pays_principale",
-                            "topographie", "altitude", "pourcent_pente", "surface", 
-                            "code_parc", "pct_mo", "pct_ph", "texture_sol", "pct_sable", "pct_argile", 
-                            "pct_limon", "pct_caillou", "pct_calcaire", "type_bordure", "annee_releve", 
-                            "largeur_bordure")]
+                            "topographie", "altitude", "surface", 
+                            "code_parc", "pct_mo", "pct_ph", "texture_sol", "annee_releve")]
+parcelle[parcelle == ""] <- NA
+parcelle <- na.omit(parcelle)
 
 
 # split first column of flore_raw
